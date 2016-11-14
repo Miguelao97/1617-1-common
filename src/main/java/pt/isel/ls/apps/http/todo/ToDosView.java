1e20a7efffa88a5d22469607547e70f95e67cde1
Pedro Felix <pmhsfelix@gmail.com>
2016-11-14 15:10:25 +0000

    adds HTTP examples

---
+++ src/main/java/pt/isel/ls/apps/http/todo/ToDosView.java
@@ -0,0 +1,35 @@
+package pt.isel.ls.apps.http.todo;
+
+import pt.isel.ls.html.HtmlElem;
+import pt.isel.ls.html.HtmlPage;
+import pt.isel.ls.todo.ToDo;
+
+import pt.isel.ls.common.Writable;
+
+public class ToDosView extends HtmlPage{
+
+    public ToDosView(Iterable<ToDo> list){
+        super("To Do list",
+            h1(text("To Do list")),            
+            todoItems(list),
+            h1(text("Create new To Do")),
+            form("POST", "/todos",
+                label("description","Description: "),
+                textInput("description"),
+                label("test", "Test: "),
+                textInput("test"),
+                new HtmlElem("input").withAttr("type","submit").withAttr("value","create")
+            )
+        );        
+    }
+    
+    private static Writable todoItems(Iterable<ToDo> list) {
+        HtmlElem ul = new HtmlElem("ul");
+        for(ToDo todo : list){
+            ul.withContent(
+                li(a(ResolveUrl.of(todo),todo.toString()))
+            );
+        }
+        return ul;
+    }    
+}
